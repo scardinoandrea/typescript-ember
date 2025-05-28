ea0ab28a61124d35ebf55cff6a8e5f0f697f55b5
Andrea Scardino <andrea.scardino.r@gmail.com>
2025-05-28 18:07:37 +0200

    fix: ember-lint config

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,27 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended',
+  plugins: ['ember-template-lint-plugin-prettier'],
+  extends: ['recommended', 'ember-template-lint-plugin-prettier:recommended'],
+  rules: {
+    'no-action-modifiers': true,
+    'no-element-event-actions': true,
+    'no-unnecessary-concat': true,
+    'sort-invocations': true,
+  },
+  overrides: [
+    {
+      files: ['**/*.{gjs,gts}'],
+      rules: {
+        'no-implicit-this': 'off',
+        prettier: 'off',
+      },
+    },
+    {
+      files: ['tests/**/*-test.{js,ts}'],
+      rules: {
+        prettier: 'off',
+      },
+    },
+  ],
 };
--- types/global.d.ts
+++ types/global.d.ts
@@ -1,1 +1,7 @@
 import '@glint/environment-ember-loose';
+
+import type EmberTruthHelpersRegistry from 'ember-truth-helpers/template-registry';
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry extends EmberTruthHelpersRegistry {}
+}
